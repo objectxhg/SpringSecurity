ba862d0809c00bb3b07da5f8e02ecc88e28446fb
objectxhg <857414521@qq.com>
2021-03-30 10:46:28 +0800

    first commit

---
+++ src/main/java/com/example/SpringSecurity/XhgApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XhgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XhgApplication.class, args);
+	}
+
+}
